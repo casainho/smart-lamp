26685695325fa8a0c92e8a47d2cdb4469da18c69
Tiago Brito <tiago.brito@beubi.com>
2012-01-25 23:19:44 +0000

    Implementation of onDraw method

--- SmartLamp/src/net/casainho/ColorPickerView.java
+++ SmartLamp/src/net/casainho/ColorPickerView.java
@@ -2,25 +2,85 @@ package net.casainho;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class ColorPickerView extends View {
 
+	private Paint mPaint;
+	private Paint mCenterPaint;
+    private int[] mColors;
+    
 	public ColorPickerView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		init();
 	}
+	private void init() {
+		
+        this.mColors = new int[] {
+            0xFFFF0000, 0xFFFF00FF, 0xFF0000FF, 0xFF00FFFF, 0xFF00FF00,
+            0xFFFFFF00, 0xFFFF0000
+        };
+        Shader s = new SweepGradient(0, 0, mColors, null);
 
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setShader(s);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(32);
+
+        mCenterPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mCenterPaint.setColor(111);//TODO: change this Init
+        mCenterPaint.setStrokeWidth(5);
+	}
+	
+	private boolean mTrackingCenter;
+    private boolean mHighlightCenter;
+    //TODO: this items must be calculated dynamically
+    private static final int CENTER_X = 100;
+    private static final int CENTER_Y = 100;
+    private static final int CENTER_RADIUS = 46;
 	@Override
 	protected void onDraw(Canvas canvas) {
-		// TODO Auto-generated method stub
-		super.onDraw(canvas);
+		
+        float r = CENTER_X - mPaint.getStrokeWidth()*0.5f;
+        
+        canvas.translate(getWidth()/2, getHeight()/2);
+
+        canvas.drawOval(new RectF(-r, -r, r, r), mPaint);
+        canvas.drawCircle(0, 0, CENTER_RADIUS, mCenterPaint);
+
+        if (mTrackingCenter) {
+            int c = mCenterPaint.getColor();
+            mCenterPaint.setStyle(Paint.Style.STROKE);
+
+            if (mHighlightCenter) {
+                mCenterPaint.setAlpha(0xFF);
+            } else {
+                mCenterPaint.setAlpha(0x80);
+            }
+            canvas.drawCircle(0, 0,
+                              CENTER_RADIUS + mCenterPaint.getStrokeWidth(),
+                              mCenterPaint);
+
+            mCenterPaint.setStyle(Paint.Style.FILL);
+            mCenterPaint.setColor(c);
+        }
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		// TODO Auto-generated method stub
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		return super.onTouchEvent(event);
 	}
 
 }
